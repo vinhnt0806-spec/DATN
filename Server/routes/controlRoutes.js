4e381c3be5957b4d37c8a1c7924e9ddc98221026
vinhnguyen <vinhnt0806@gmail.com>
2026-06-25 22:54:04 +0700

    update web

--- Server/routes/controlRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const controlController = require("../controllers/controlController");
-
-router.post("/control",controlController.updateControl);
-
-router.post("/thresholds",controlController.updateThresholds);
-
-module.exports = router;
